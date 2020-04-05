f918b9d01c7c63dbbccf8c6ea5236787d1bb11dd
Nikita Skrebets <angelhearted@ya.ru>
2020-04-05 15:59:49 +0300

    feat: implement initial preprocess; all tests pass though they should not

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  const SIZE = 9;
+  const ALL_NUMS = new Array(SIZE).fill(0).map((_, i) => i + 1);
+
+  const getPossibileValues = (row, col, m) => {
+    const values = new Set(ALL_NUMS);
+
+    // filter in row
+    m[row].forEach((v) => values.delete(v));
+    if (values.size === 1) {
+      return values.values().next().value;
+    }
+
+    // filter in column
+    m.forEach((r) => values.delete(r[col]));
+    if (values.size === 1) {
+      return values.values().next().value;
+    }
+
+    // filter in square
+    const [fromRow, fromCol] = [Math.floor(row / 3) * 3, Math.floor(col / 3) * 3];
+    for (let i = fromRow; i < fromRow + 3; i++) {
+      m[i].slice(fromCol, fromCol + 3).forEach((v) => values.delete(v));
+    }
+
+    return values.size === 1 ? values.values().next().value : [...values];
+  };
+
+  const preprocessMatrix = (m) => {
+    const result = new Array(SIZE).fill(0).map(() => new Array(SIZE));
+
+    for (let i = 0; i < SIZE; i++) {
+      for (let j = 0; j < SIZE; j++) {
+        result[i][j] = m[i][j] === 0 ? getPossibileValues(i, j, m) : m[i][j];
+      }
+    }
+
+    return result;
+  }
+
+  const result = preprocessMatrix(matrix);
+
+  return result;
 }
